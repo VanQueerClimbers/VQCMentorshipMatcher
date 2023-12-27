3d7b044996c0e39e7cfa0559576ad3a3c30c5533
Michal Dacko <michal@michaldacko.com>
2023-12-27 01:14:25 -0800

    Fix scrolling bug in modals

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -11,7 +11,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
       <div className="absolute bg-gray-800 opacity-75 inset-0" onClick={onClose}></div>
-      <div className="bg-white rounded-lg p-8 z-10 divide-y divide-solid divide-purple-900">
+      <div className="bg-white rounded-lg p-8 z-10 divide-y divide-solid divide-purple-900 max-h-full overflow-y-auto">
         {children}
         <button
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
