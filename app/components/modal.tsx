c3cb32ea38f9e2fe7181be12eb5e04ad2b7eb275
Michal Dacko <michal@michaldacko.com>
2024-01-04 02:41:06 -0800

    Close modal with escape

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 
 interface ModalProps {
   isOpen: boolean;
@@ -7,10 +7,22 @@ interface ModalProps {
 }
 
 const Modal = ({ isOpen, onClose, children } : ModalProps) => {
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, [isOpen])
+
   if (!isOpen) return null;
 
+  const handleEsc = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (event.key === 'Escape') {
+      onClose();
+    }
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+    <dialog ref={ref} className="fixed top-0 left-0 w-full h-full flex items-center justify-center" onKeyDown={handleEsc}>
       <div className="absolute bg-gray-800 opacity-75 inset-0" onClick={onClose}></div>
       <div className="bg-white rounded-lg p-8 z-10 divide-y divide-solid divide-purple-900 max-h-full overflow-y-auto">
         {children}
@@ -21,7 +33,7 @@ const Modal = ({ isOpen, onClose, children } : ModalProps) => {
           Close
         </button>
       </div>
-    </div>
+    </dialog>
   );
 };
 
--- app/components/personview.tsx
+++ app/components/personview.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import { useDrag } from "react-dnd";
 import { Person, Mentor, Team } from '../lib/models'
 import Modal from './modal'
